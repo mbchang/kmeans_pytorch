d4079a8223a0bc254ce64310fbcdecdf87291d92
mbchang <mbchang@fb.com>
2022-03-24 12:02:54 -0700

    BUG FIX: reinitialzie when we have an empty cluster

--- kmeans.py
+++ kmeans.py
@@ -8,10 +8,37 @@ def forgy(X, n_clusters):
 	return initial_state
 
 
+# def lloyd(X, k, metric, tol=1e-4):
+
+# 	initial_state = forgy(X, k)
+
+# 	while True:
+# 		dis = metric(X, initial_state)
+
+# 		choice_cluster = torch.argmin(dis, dim=1)
+
+# 		initial_state_pre = initial_state.clone()
+
+# 		for index in range(k):
+# 			selected = torch.nonzero(choice_cluster==index).squeeze()
+
+# 			selected = torch.index_select(X, 0, selected)
+# 			initial_state[index] = selected.mean(dim=0)
+		
+
+# 		center_shift = torch.sum(torch.sqrt(torch.sum((initial_state - initial_state_pre) ** 2, dim=1)))
+
+# 		if center_shift ** 2 < tol:
+# 			break
+
+# 	return choice_cluster, initial_state
+
+
 def lloyd(X, k, metric, tol=1e-4):
 
 	initial_state = forgy(X, k)
 
+	i = 0
 	while True:
 		dis = metric(X, initial_state)
 
@@ -23,10 +50,16 @@ def lloyd(X, k, metric, tol=1e-4):
 			selected = torch.nonzero(choice_cluster==index).squeeze()
 
 			selected = torch.index_select(X, 0, selected)
+
+            # https://github.com/subhadarship/kmeans_pytorch/issues/16
+			if selected.shape[0] == 0:
+				selected = X[torch.randint(len(X), (1,))]
+
 			initial_state[index] = selected.mean(dim=0)
 		
-
 		center_shift = torch.sum(torch.sqrt(torch.sum((initial_state - initial_state_pre) ** 2, dim=1)))
+		print(f'iteration {i}\tcenter shift {center_shift}\ttolerance {tol}')
+		i += 1
 
 		if center_shift ** 2 < tol:
 			break
